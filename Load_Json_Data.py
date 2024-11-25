c25db00c2ee3ecabbd7e204715f58adc05027884
ff <gg>
2024-11-25 23:56:08 +0530

    Using Json module and pandas

--- Load_Json_Data.py
+++ Load_Json_Data.py
@@ -3,4 +3,10 @@ import json
 with open("GenreData.json") as Jsondatafile:
     data1 = json.load(Jsondatafile)
     pan = pd.DataFrame(data1)
-print(pan)
+#Using the pandas to display the items.
+for genre in data1["genres"]:
+    print(genre["name"])
+    print(genre["description"])
+#We can also use json module to display the items.
+for genre1 in data1["genres"]:
+    print(genre1["name"])
